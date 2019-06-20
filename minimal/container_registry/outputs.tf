0500e72f8caf80df6a8ba9b214154608ee3235ab
Thorsten Hans <thorsten.hans@gmail.com>
2019-06-20 11:53:49 +0200

    fix: remove odd comma in outputs.tf

--- minimal/container_registry/outputs.tf
+++ minimal/container_registry/outputs.tf
@@ -1,4 +1,4 @@
 output "login_server" {
-  value = "${azurerm_container_registry.container_registry.login_server}",
+  value = "${azurerm_container_registry.container_registry.login_server}"
   description = "The URL that can be used to log into the container registry."
 }
