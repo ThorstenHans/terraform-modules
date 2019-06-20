676335f174c59111d402d72e0a334c1be3f78c7c
Thorsten Hans <thorsten.hans@gmail.com>
2019-06-20 12:05:43 +0200

    docs: add description for app_insights output

--- minimal/application_insights/outputs.tf
+++ minimal/application_insights/outputs.tf
@@ -1,3 +1,4 @@
 output "instrumentation_key" {
-  value = "${azurerm_application_insights.application_insight.instrumentation_key}"
+  value       = "${azurerm_application_insights.application_insight.instrumentation_key}"
+  description = "The Application Insights Instrumentation Key"
 }
