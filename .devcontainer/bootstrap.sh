a97597d428162b0303efcb0d139585365553e566
Thorsten Hans <thorsten.hans@gmail.com>
2019-11-06 11:12:36 +0000

    devcontainer

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install wget unzip -y
+cd /home/vsonline
+wget https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip
+unzip terraform_0.12.13_linux_amd64.zip
+sudo ln -s /home/vsonline/terraform /usr/local/bin/terraform
+sudo ln -s /home/vsonline/terraform /usr/local/bin/t
+cd /home/vsonline/workspace
+echo -n "Terraform installed and linked as terraform and t"
+t --version
